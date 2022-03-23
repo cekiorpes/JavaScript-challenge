d674ba6ad49df201a3b10d982f4539d05b60c93b
Caroline Kiorpes <cekiorpes@gmail.com>
2022-03-22 23:00:40 -0400

    Working on reading in data

--- static/js/app.js
+++ static/js/app.js
@@ -1,3 +1,6 @@
 // Read in data
-let data = d3.json("samples.json");
-console.log(data);
+let data = d3.json("samples.json", function() {
+    let subjectID = Object.values(data.names);
+    let demographic = Object.values(data.metadata);
+    let sample_data = Object.values(data.samples);
+});
