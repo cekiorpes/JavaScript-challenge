b1181df7e7399a57ad13126ebe40a17611556760
Caroline Kiorpes <cekiorpes@gmail.com>
2022-03-24 15:15:47 -0400

    Bar graph displays correctly

--- static/js/app.js
+++ static/js/app.js
@@ -1,5 +1,5 @@
 // Read in data - due to browser issues, I converted the samples file to JavaScript
-// let data = d3.json("samples.json", function() {
+// let data = d3.json("samples.json").then(function(samples) {
 //     console.log(data);
 // });
 
@@ -11,31 +11,43 @@ console.log(demographic);
 let sample_data = Object.values(data.samples);
 console.log(sample_data);
 
+//Put test subject names in dropdown menu
+let ddmenu = d3.select("#selDataset")
+
+
 // Create horizontal bar chart - not displaying bar chart, but data displays correctly in console
 ID = "940";
-let filteredSample = sample_data.filter(patient => patient.id === ID);
+let filteredSample = sample_data.filter(patient => patient.id === ID)[0];
 console.log(filteredSample);
 
-let patient_values = filteredSample.map(p => p.sample_values.slice(0, 10));
+// let sorted_filtered = filteredSample.sort( (p1, p2) => p2.sample_values - p1.sample_values);
+// console.log(sorted_filtered);
+
+let patient_values = filteredSample.sample_values.slice(0, 10).reverse();
 console.log(patient_values);
-let patient_otu_ids = filteredSample.map(p => p.otu_ids.slice(0, 10));
+let patient_otu_ids = filteredSample.otu_ids.slice(0, 10).reverse();
 console.log(patient_otu_ids);
-let patient_labels = filteredSample.map(p => p.otu_labels.slice(0, 10));
+let patient_labels = filteredSample.otu_labels.slice(0, 10).reverse();
 console.log(patient_labels);
 
-let hbar = [{
+let hbar = [
+    {
     x: patient_values,
     y: patient_otu_ids.map(otu => `OTU ${otu}`),
     text: patient_labels,
     type: "bar",
     orientation: "h",
-}];
+    }
+];
 
 let layout = {
     title: "Bacteria in Subject Belly Button",
 };
 
 Plotly.newPlot("bar", hbar, layout);
+
+//Create bubble chart
+
 
 
 
