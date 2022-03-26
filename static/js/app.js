bf977bbd50d088fd288f5bc3af2d3c3bdf4c4eff
Caroline Kiorpes <cekiorpes@gmail.com>
2022-03-25 22:39:42 -0400

    Successfully read in data from json file using D3; added in data for demographic table

--- static/js/app.js
+++ static/js/app.js
@@ -1,92 +1,121 @@
-// Read in data - due to browser issues, I converted the samples file to JavaScript
-// d3.json("/samples.json").then(function(data) {
-//     console.log(data);
+// Read in data
+d3.json("samples.json").then(function(data) {
+    console.log(data);
 
-//     let subjectIDs = data.names;
-//     console.log(subjectIDs);
+    let subjectIDs = data.names;
+    console.log(subjectIDs);
 
-//     let demographic = data.metadata;
-//     console.log(demographic);
+    //Put test subject names in dropdown menu
+    let ddmenu = d3.select("#selDataset");
+    for (subjectID of subjectIDs) {
+    ddmenu.append("option").text(subjectID).property("value", subjectID);
+    };
+    // function to change ID .on("change", ID)?, then call chart and demographic table functions?
+});
 
-//     let sample_data = data.samples;
-//     console.log(sample_data);
-// });
 
 // Read in data from samples.js file
-let subjectIDs = Object.values(data.names);
-console.log(subjectIDs);
-let demographic = Object.values(data.metadata);
-console.log(demographic);
-let sample_data = Object.values(data.samples);
-console.log(sample_data);
+// let subjectIDs = Object.values(data.names);
+// console.log(subjectIDs);
+// let demographic = Object.values(data.metadata);
+// console.log(demographic);
+// let sample_data = Object.values(data.samples);
+// console.log(sample_data);
 
-//Put test subject names in dropdown menu
-let ddmenu = d3.select("#selDataset");
-for (subjectID of subjectIDs) {
-    ddmenu.append("option").text(subjectID).property("value", subjectID);
-};
-// function to change ID .on("change", ID)?, then call chart and demographic table functions?
 
-// Create horizontal bar chart
-ID = "940";
-//Filter data by ID number
-let filteredSample = sample_data.filter(patient => patient.id === ID)[0];
-console.log(filteredSample);
+// Create charts based on ID
+//Read in sample data
+d3.json("samples.json").then(function(data) {
+    console.log(data);
 
-//Find sample_values for specific test subject
-let patient_values = filteredSample.sample_values;
-console.log(patient_values);
-//Find otu_ids for specific test subject
-let patient_otu_ids = filteredSample.otu_ids;
-console.log(patient_otu_ids);
-//Find otu_labels for specific test subject
-let patient_labels = filteredSample.otu_labels;
-console.log(patient_labels);
+    let sample_data = data.samples;
+    console.log(sample_data);
 
-//Data for bar chart
-let hbar = [
-    {
-    x: patient_values.slice(0, 10).reverse(),
-    y: patient_otu_ids.slice(0, 10).reverse().map(otu => `OTU ${otu}`),
-    text: patient_labels.slice(0, 10).reverse(),
-    type: "bar",
-    orientation: "h",
-    }
-];
+    let ID = "940";
+    //Filter data by ID number
+    let filteredSample = sample_data.filter(patient => patient.id === ID)[0];
+    console.log(filteredSample);
 
-//Layout for bar chart
-let layout = {
-    title: "Bacteria in Subject Belly Button",
-};
+    //Find sample_values for specific test subject
+    let patient_values = filteredSample.sample_values;
+    console.log(patient_values);
+    //Find otu_ids for specific test subject
+    let patient_otu_ids = filteredSample.otu_ids;
+    console.log(patient_otu_ids);
+    //Find otu_labels for specific test subject
+    let patient_labels = filteredSample.otu_labels;
+    console.log(patient_labels);
 
-//Render the plot to the bar id tag
-Plotly.newPlot("bar", hbar, layout);
-
-//Create bubble chart
-//Data for the bubble chart
-let bubble = [
-    {
-        x: patient_otu_ids,
-        y: patient_values,
-        text: patient_labels,
-        mode: "markers",
-        marker: {
-            size: patient_values,
-            color: patient_otu_ids,
+    //Data for horizontal bar chart
+    let hbar = [
+        {
+        x: patient_values.slice(0, 10).reverse(),
+        y: patient_otu_ids.slice(0, 10).reverse().map(otu => `OTU ${otu}`),
+        text: patient_labels.slice(0, 10).reverse(),
+        type: "bar",
+        orientation: "h",
         }
-    }
-];
+    ];
 
-//Layout for the bubble chart
-let bubble_layout = {
-    title: "Belly Button Sample Bacteria",
-    xaxis: {
-        title: "OTU IDs",
-    }
-};
+    //Layout for bar chart
+    let layout = {
+        title: "Bacteria in Subject Belly Button",
+    };
 
-//Render the chart to the bubble id tag
-Plotly.newPlot("bubble", bubble, bubble_layout);
+    //Render the plot to the bar id tag
+    Plotly.newPlot("bar", hbar, layout);
 
+    //Create bubble chart
+    //Data for the bubble chart
+    let bubble = [
+        {
+            x: patient_otu_ids,
+            y: patient_values,
+            text: patient_labels,
+            mode: "markers",
+            marker: {
+                size: patient_values,
+                color: patient_otu_ids,
+            }
+        }
+    ];
+
+    //Layout for the bubble chart
+    let bubble_layout = {
+        title: "Belly Button Sample Bacteria",
+        xaxis: {
+            title: "OTU IDs",
+        }
+    };
+
+    //Render the chart to the bubble id tag
+    Plotly.newPlot("bubble", bubble, bubble_layout);
+});
+
+
+//Put demographic information in chart
+let demo = d3.select("#sample-metadata");
+let ID = "940";
+
+//Read in demographic data
+d3.json("samples.json").then(function(data) {
+    console.log(data);
+
+    let demographic = data.metadata;
+    console.log(demographic);
+
+    //Filter demographic data by ID number
+    let filteredDemographic = demographic.filter(object => object.id === parseInt(ID))[0];
+    console.log(filteredDemographic);
+
+    //Read each key:value pair in filtered data as its own array
+    let rows = Object.entries(filteredDemographic);
+    console.log(rows);
+
+    //Loop through each pair and append to demographic table
+    for (row of rows) {
+        demo.append("p").text(`${row[0]}: ${row[1]}`)
+    };
+});
 
 
